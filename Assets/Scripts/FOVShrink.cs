cb050f75b7b461b5307ad26d1aa76f7b8a1ad91d
rasbot <nathan.f.rasmussen@gmail.com>
2020-06-16 16:27:17 -0700

    initial project commit

---
+++ Assets/Scripts/FOVShrink.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.ImageEffects;
+
+public class FOVShrink : MonoBehaviour {
+    public float simulatedFOV;
+
+    public float IPD;
+    public float SCREEN_DISTANCE;
+
+    public VignetteAndChromaticAberration[] fovControls;
+
+	// Update is called once per frame
+	void Update () {
+        float maxFOV = Mathf.Rad2Deg * Mathf.Atan2(IPD / 2f, SCREEN_DISTANCE);
+        foreach (VignetteAndChromaticAberration fov in fovControls)
+            fov.intensity = 1f - Mathf.Clamp01(simulatedFOV / maxFOV);		
+	}
+}
---
+++ Assets/Scripts/fadescript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class fadescript : MonoBehaviour {
+
+    public GameObject fadeObject;
+    GameLogic gameLogic;
+    
+	// Use this for initialization
+	void Start ()
+    {
+
+    }
+    IEnumerator StartOver()
+   {
+        yield return new WaitForSeconds(2.5f);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+    // Update is called once per frame
+    void Update ()
+    {
+            StartCoroutine(StartOver());
+	}
+}
